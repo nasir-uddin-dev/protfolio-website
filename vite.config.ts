5f9832c4bd64ea2fb26fbc9528ba45e5b5652ad2
PC <nasiruddin3954@gmail.com>
2026-06-16 20:53:48 +0600

    add vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/protfolio-website/',
+  base: process.env.VITE_BASE_PATH || '//protfolio-website',
   plugins: [react()],
 })
